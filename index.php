159622cb5fb0c71bf1b43d213050a9a39bd308b7
Evgenii Mishkin <emishkin@mail.greenriver.edu>
2020-01-15 14:29:28 -0500

    Added config file and index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+     * Author: Evgenii Mishkin
+     * Date: 01/15/2020
+     * URL: http://emishkin.greenriverdev.com/328/chicken/index.php
+     * Description:
+     */
+
+    // Turn on error reporting - this is critical!
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
